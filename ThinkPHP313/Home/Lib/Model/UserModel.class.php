7edc0f3ce992b866a4c8b8bece2eeaadb274fac0
vickyQQ <supervip8@163.com>
2017-03-29 18:25:42 +0800

    11

--- ThinkPHP313/Home/Lib/Model/UserModel.class.php
+++ ThinkPHP313/Home/Lib/Model/UserModel.class.php
@@ -2,6 +2,7 @@
 class UserModel extends Model{
 	protected $patchValidate=true;
 	protected $_validate = array(
+		array('uname', 'require', '用户名称不能为空！', 1, 'regex'),
 	    array('uname','','帐号名称已经存在！',0,'unique',1), // 在新增的时候验证name字段是否唯一
 	    array('upwd','6-50','密码长度必须6位！',0,'length',1), // 密码验证
 	    array('repassword','upwd','密码不一致！',0,'confirm',1), // 密码验证
